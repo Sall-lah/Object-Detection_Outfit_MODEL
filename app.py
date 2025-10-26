ee2b540e892aa0ed6c8b67bae0eced0d83222e69
Sall-lah <ikhsan.ibnu.abdulah@gmail.com>
2025-10-26 23:41:00 +0700

    update
    new dataset (cant detect white or black coloured)

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import os
+import cv2
+import shutil
+import tqdm
+import glob
+
 from ultralytics import YOLO
 
-# Load pretrained model (e.g. YOLOv8s)
-model = YOLO("yolov8s.pt")
+# load pre-trained model
+detection_model = YOLO("yolov8n.pt")
 
-# Run detection on an image or video
-results = model("image.jpg", show=True)
+# choose random image
+# img = random.choice(os.listdir(images_path))
 
-# Print results
-for r in results:
-    print(r.boxes.xyxy)  # bounding box coordinates
+i=detection_model.predict(source='https://i.imgur.com/7DbG6Kt.jpeg', conf=0.5, save=True, line_thickness=2, hide_labels=False)
+
+im = plt.imread('/kaggle/working/runs/detect/predict/GRdCC.jpg')
+plt.figure(figsize=(20,10))
+plt.axis('off')
+plt.imshow(im)
