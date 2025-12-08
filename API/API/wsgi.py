c2c9399da7412f2d97432e292828d1c45873f101
Sall-lah <ikhsan.ibnu.abdulah@gmail.com>
2025-12-08 21:40:17 +0700

    Update WSGI

--- API/API/wsgi.py
+++ API/API/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API.settings")
 
+os.environ["YOLO_CONFIG_DIR"] = "/tmp/ultralytics_config"
+
 application = get_wsgi_application()
